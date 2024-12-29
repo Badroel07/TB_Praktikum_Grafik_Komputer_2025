e4685d889298f9bb3e551416815a5534fc851eee
Chikal Ubaidillah <muhamadchikal12@gmail.com>
2024-12-29 23:54:45 +0700

    update rotasi, penyempurnaan objek Susu_Kaleng dan Teh_Kotak

--- Minuman_Kemasan_3D_OpenGL.cpp
+++ Minuman_Kemasan_3D_OpenGL.cpp
@@ -9,10 +9,13 @@ void drawCartecius();
 void display();
 void reshape(int w, int h);
 void updateRotation();
-GLuint loadTexture(const char* filename);
 
+// variabel global
+GLuint loadTexture(const char* filename);
+GLuint texture_sisiKaleng, texture_atas, texture_bawah, tekstur_kaleng;
 GLuint textureFront, textureBack, textureLeft, textureRight, textureTop, textureBottom;
 float angleX = 0.0f, angleY = 0.0f; // Variabel rotasi
+float tick = 0.0;
 
 // Fungsi utama
 int main(int argc, char** argv) {
@@ -24,13 +27,18 @@ int main(int argc, char** argv) {
     glewInit();
     glEnable(GL_DEPTH_TEST);
 
-    // Memuat tekstur untuk setiap sisi
+    // Tekstur Teh Kotak
     textureFront = loadTexture("assets/Teh_Kotak/front.png");
     textureBack = loadTexture("assets/Teh_Kotak/back.png");
     textureLeft = loadTexture("assets/Teh_Kotak/left.png");
     textureRight = loadTexture("assets/Teh_Kotak/right.png");
     textureTop = loadTexture("assets/Teh_Kotak/top.png");
     textureBottom = loadTexture("assets/Teh_Kotak/bottom.png");
+    // Tekstur Susu Kaleng
+    texture_sisiKaleng = loadTexture("assets/Susu_Kaleng/sisi.png");
+    texture_atas = loadTexture("assets/Susu_Kaleng/atas.png");
+    texture_bawah = loadTexture("assets/Susu_Kaleng/bawah.png");
+    tekstur_kaleng = loadTexture("assets/Susu_Kaleng/tekstur_kaleng.png");
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
@@ -80,6 +88,45 @@ GLuint loadTexture(const char* path) {
     return -1;
 }
 
+void SusuKaleng() {
+    GLUquadric* object = gluNewQuadric();
+    gluQuadricTexture(object, GL_TRUE);
+    gluQuadricNormals(object, GLU_SMOOTH);
+
+    float radius = 0.17f; // Sesuaikan diameter susu kaleng (lebar Teh Kotak adalah 0.6, jadi radius 0.2)
+    float height = 0.4f; // Sesuaikan tinggi susu kaleng (tinggi Teh Kotak adalah 1.0, jadi kaleng lebih kecil)
+
+    // Cylinder (sisi kaleng)
+    glBindTexture(GL_TEXTURE_2D, texture_sisiKaleng);
+    gluCylinder(object, radius, radius, 0.6f, 50, 4);
+
+    // Top (penutup atas)
+    glPushMatrix();
+    glTranslatef(0, 0, 0.62); // Pindahkan ke atas kaleng
+    glBindTexture(GL_TEXTURE_2D, texture_atas);
+    gluDisk(object, 0, radius, 50, 2);
+    glPopMatrix();
+
+    // Bottom (penutup bawah)
+    glPushMatrix();
+    glTranslatef(0, 0, 0); // Pindahkan ke bawah kaleng
+    glBindTexture(GL_TEXTURE_2D, texture_bawah);
+    gluDisk(object, 0, radius, 40, 1);
+    glPopMatrix();
+}
+
+void SusuKaleng2() {
+    GLUquadric* object = gluNewQuadric();
+    gluQuadricTexture(object, GL_TRUE);
+    gluQuadricNormals(object, GLU_SMOOTH);
+
+    float radius = 0.2f; // Sesuaikan diameter susu kaleng (lebar Teh Kotak adalah 0.6, jadi radius 0.2)
+    float height = 0.4f; // Sesuaikan tinggi susu kaleng (tinggi Teh Kotak adalah 1.0, jadi kaleng lebih kecil)
+
+    // Cylinder (sisi kaleng)
+    glBindTexture(GL_TEXTURE_2D, tekstur_kaleng);
+    gluCylinder(object, 0.1699f, 0.1699f, 0.6159f, 50, 4);
+}
 // Fungsi untuk menggambar kubus (kemasan minuman) dengan tekstur
 void TehKotak() {
     glEnable(GL_TEXTURE_2D);
@@ -153,11 +200,27 @@ void display() {
     // Mengatur kamera
     gluLookAt(2.0, 2.0, -2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
     drawCartecius();
-    // Rotasi objek
+
+    // Rotasi objek utama (Teh Kotak)
+    glPushMatrix();
     glRotatef(angleX, 1.0f, 0.0f, 0.0f);
     glRotatef(angleY, 0.0f, 1.0f, 0.0f);
-    // Menggambar Teh kotak
+
+    // Menggambar Teh Kotak
     TehKotak();
+    glPopMatrix();
+
+    // Rotasi dan posisi objek kedua (Susu Kaleng)
+    glPushMatrix();
+    glEnable(GL_TEXTURE_2D);
+    glTranslatef(1.0, 0.0, 0.0); // Posisikan di samping Teh Kotak
+    glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
+    glRotatef(angleY, 0.0f, 0.0f, 1.0f);
+
+    // Menggambar Susu Kaleng
+    SusuKaleng2();
+    SusuKaleng();
+    glPopMatrix();
 
     glutSwapBuffers();
 }
